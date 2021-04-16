d6e7a26f05c8805b7fa8023d7f1c621f77eda24e
snooh <snooh@sfu.ca>
2021-04-15 21:19:46 -0700

    Comment out audio code for basic testing

--- bot.py
+++ bot.py
@@ -66,58 +66,58 @@ async def whoami(ctx):
 async def clear(ctx, amount=3):
     await ctx.channel.purge(limit=amount)
 
-# Actual music playing code
-@client.command(name="join", help="Mista joins the party!")
-async def join(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send("Mista thinks {} is not connected to a voice channel! :(".format(ctx.message.author.name))
-        return
-    else:
-        channel = ctx.message.author.voice.channel
-    await channel.connect()
+# # Actual music playing code
+# @client.command(name="join", help="Mista joins the party!")
+# async def join(ctx):
+#     if not ctx.message.author.voice:
+#         await ctx.send("Mista thinks {} is not connected to a voice channel! :(".format(ctx.message.author.name))
+#         return
+#     else:
+#         channel = ctx.message.author.voice.channel
+#     await channel.connect()
 
-@client.command(name="play", help="Mista starts playing! #play 'song_url'")
-async def play(ctx, url : str):
-    voice_channel = discord.utils.get(ctx.guild.voice_channels)
-    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-    if not voice.is_connected():
-        await ctx.send("Mista is playing!")
-        await voice_channel.connect()
-    else:
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=client.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(
-                executable="ffmpeg.exe", source=filename))
-        await ctx.send('**Now playing:** {}'.format(filename))
+# @client.command(name="play", help="Mista starts playing! #play 'song_url'")
+# async def play(ctx, url : str):
+#     voice_channel = discord.utils.get(ctx.guild.voice_channels)
+#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+#     if not voice.is_connected():
+#         await ctx.send("Mista is playing!")
+#         await voice_channel.connect()
+#     else:
+#         async with ctx.typing():
+#             filename = await YTDLSource.from_url(url, loop=client.loop)
+#             voice_channel.play(discord.FFmpegPCMAudio(
+#                 executable="ffmpeg.exe", source=filename))
+#         await ctx.send('**Now playing:** {}'.format(filename))
 
-@client.command(name="leave", help="Mista leaves...")
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("Mista was never in the voice channel...")
+# @client.command(name="leave", help="Mista leaves...")
+# async def leave(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_connected():
+#         await voice.disconnect()
+#     else:
+#         await ctx.send("Mista was never in the voice channel...")
 
-@client.command(name="pause", help="Mista pauses song!")
-async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Mista can't pause a non-existent song...")
+# @client.command(name="pause", help="Mista pauses song!")
+# async def pause(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_playing():
+#         voice.pause()
+#     else:
+#         await ctx.send("Mista can't pause a non-existent song...")
 
-@client.command(name="resume", help="Mista resumes song!")
-async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-    else:
-        await ctx.send("Mista can't resume a non-existent song...")
+# @client.command(name="resume", help="Mista resumes song!")
+# async def resume(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     if voice.is_paused():
+#         voice.resume()
+#     else:
+#         await ctx.send("Mista can't resume a non-existent song...")
 
-@client.command(name="stop", help="Mista stops!")
-async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
+# @client.command(name="stop", help="Mista stops!")
+# async def stop(ctx):
+#     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     voice.stop()
 
 if __name__ == "__main__":
     client.run(os.environ['TOKEN'])
